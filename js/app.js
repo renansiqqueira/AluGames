0222b99df4d234723b74315e406a71af867d28cb
Renan <rsiqueira78@gmail.com>
2025-06-10 13:34:03 -0300

    implementar confirmação de devolução

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
 function alterarStatus(id) {
-let gameClicado = document.getElementById(`game-${id}`);
-let capaGame = gameClicado.querySelector('.dashboard__item__img');
-let nomeGame = gameClicado.querySelector('.dashboard__item__name');
-let botao = gameClicado.querySelector('.dashboard__item__button');
+    let gameClicado = document.getElementById(`game-${id}`);
+    let capaGame = gameClicado.querySelector('.dashboard__item__img');
+    //let nomeGame = gameClicado.querySelector('.dashboard__item__name');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
 
-if(capaGame.classList.contains('dashboard__item__img--rented')){
-    capaGame.classList.remove('dashboard__item__img--rented');
-    botao.classList.remove('dashboard__item__button--return');
-    botao.textContent = 'Alugar';
 
-}else{
-    capaGame.classList.add('dashboard__item__img--rented');
-    botao.classList.add('dashboard__item__button--return');
-    botao.textContent = 'Devolver';
-}
+    if (capaGame.classList.contains('dashboard__item__img--rented')) {
+
+        //Confirmação de devolução
+        let confirmacao = prompt('Deseja confirmar devolução (S/N)?');
+        if (confirmacao == 's' || confirmacao == 'S') {
+            capaGame.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        } else if(confirmacao == 'n' || confirmacao == 'N') {
+            return;
+        }
+
+
+    } else {
+        capaGame.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
 
 }
